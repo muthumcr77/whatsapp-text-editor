cdb08b761262ab904da3725fa3e71c1810464a8b
MuthuvelM <muthumcr77@gmail.com>
2022-08-28 14:17:50 +0530

    Added copy to clipboard action

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-// $(function(){
-//   alert("Hello World!");
-//   $("#copyButton").click(function(){
-//     $(this).hide();
-//     alert("Bye!");
-//   })
-// });
 $(function(){
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-  $(tooltipList).tooltip();
+  $(tooltipList).tooltip("show");
 });
+
+const copyToClipboard = () => {
+  var txt = document.getElementById("myText").value;
+  navigator.clipboard.writeText(txt).then(() => {
+    alert("Copied to clipboard!");
+  });
+}
+

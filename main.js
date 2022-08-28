795a7c6f1aee292446244a27f9718d4d2a9b0911
MuthuvelM <muthumcr77@gmail.com>
2022-08-28 10:12:14 +0530

    UI ready commit

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+// $(function(){
+//   alert("Hello World!");
+//   $("#copyButton").click(function(){
+//     $(this).hide();
+//     alert("Bye!");
+//   })
+// });
 $(function(){
-  alert("Hello World!");
-  $("#copyButton").click(function(){
-    $(this).hide();
-    alert("Bye!");
-  })
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  $(tooltipList).tooltip();
 });

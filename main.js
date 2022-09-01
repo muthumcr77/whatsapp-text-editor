4f492953ca6ce3caddce87ff4362acf605a473b5
MuthuvelM <muthumcr77@gmail.com>
2022-09-01 08:28:10 +0530

    Converted vanilla JS to JQuery

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 // Selected text global scope
 let fullText = "";
 let selText = "";
+let oldString = "";
+let newString = "";
+let editedString = ""; 
+let formattedString = "";
+
 
 // JQuery 
 $(function(){
   // Initialize clipboard target
   var clipboard = new ClipboardJS('#copyButton', {
     container: document.getElementById("myText")
-  });  
+  });    
   
   clipboard.on('success', function(e) {
     console.info('Action:', e.action);
@@ -15,103 +20,62 @@ $(function(){
     console.info('Trigger:', e.trigger);
     
     e.clearSelection();
-  });    
+  });      
   
   clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
-  });    
+  });      
   
   // Initialize tooltips
   const tooltipTriggerList = $('[data-bs-toggle="tooltip"]')
   tooltipTriggerList.each(function(){
     new bootstrap.Tooltip(this);
-  })
+  })  
   
-  $("textarea").focus(function(){
-    fullText = $("#myText").val();
-    console.log(fullText);
-  });
+  // Get fullText on input
+  $("#myText").on("input", function(){
+    fullText = $(this).val();
+    console.log("Full Text: " + fullText);
+  });  
+
   
+  // Get selText on mouseout
+  $("#myText").on("mouseout", function(){
+    selText = window.getSelection().toString();
+    console.log("Selected Text: " + selText);
+    // Trim selected text if any spaces
+    oldString = selText.trim();
+    console.log("Old string: " + oldString);
+    newString = fullText.replace(oldString, "");
+    console.log("New string: " + newString);
+  });  
+  
+
   // Format type selected
   $(".ftBtn").click(function(){
     var myId = $(this).attr("id");
     console.log("My ID: " + myId);
     switch (myId) {
       case "i-Btn":
-        $("#myText").val(`_${selText}_`);
+        editedString = `_${selText}_`;
         break;
       case "b-Btn":
-        $("#myText").val(`*${selText}*`);
+        editedString = `*${selText}*`;
         break;
       case "s-Btn":
-        $("#myText").val(`~${selText}~`);
+        editedString = `~${selText}~`;
         break;
       case "m-Btn":
-        $("#myText").val(`\`\`\`${selText}\`\`\``);
+        editedString = `\`\`\`${selText}\`\`\``;
         break;
     }
+    console.log("Edited string: " + editedString);
+    formattedString = newString.concat(editedString);
+    console.log("Formatted string: " + formattedString);
+    // Change element's value to formattedString
+    $("#myText").val(formattedString);
   });
 });
-        
-        
-// Copy formatted message to clipboard
-const copyToClipboard = () => {
-  var txt = document.getElementById("myText").value;
-  if (txt == "")
-  alert('Your message is empty!');
-  else
-  navigator.clipboard.writeText(txt).then(() => {
-    alert("Copied to clipboard!");
-  });
-}
-
-// Get selected text from document
-const getSelectionText = () => {
-  var text = "";
-  var activeEl = document.activeElement;
-  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-  if (
-    (activeElTagName == "textarea") || (activeElTagName == "input" &&
-    /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
-    (typeof activeEl.selectionStart == "number")
-  ) {
-      text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-  } else if (window.getSelection) {
-      text = window.getSelection().toString();
-  }
-  return text;
-}
-
-// Assign selected text upon event trigger
-document.onmouseup = document.onkeyup = document.onselectionchange = function() {
-  selText = getSelectionText();
-  console.log("Checking selected text: " + selText);
-};
-
-// Change to JQuery
-let fullTxt = "";
-let selectTxt = "";
-
-const element = document.getElementById("myId");
-
-element.addEventListener("input", function(){
-  fullTxt = element.value;
-});
-
-element.addEventListener("mouseout", function(){
-  console.log("Full text: " + fullTxt);
-  selectTxt = window.getSelection().toString();
-  console.log("Selected text: " + selectTxt);
-});
-
-// Action to compare FullTxt & selectTxt
-// Trim selected text if any spaces
-let oldString = selectTxt.trim();
-let newString = fullText.replace(oldString, "");
-let editedString = ""; // Check switch condition
-let formattedString = newString.concat(editedString);
-
-// Change text area's innerText/innerHtml to formattedString
 
 
